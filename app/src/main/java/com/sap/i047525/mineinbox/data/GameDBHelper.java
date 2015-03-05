6bf0bbada98ec90c34360366816a92db4dcf1528
Krisztian Kertesz <krisztian.kertesz@sap.com>
2015-03-05 16:48:52 +0100

    GameDbHelper push

--- app/src/main/java/com/sap/i047525/mineinbox/data/GameDBHelper.java
+++ app/src/main/java/com/sap/i047525/mineinbox/data/GameDBHelper.java
@@ -1,31 +1,30 @@
 package com.sap.i047525.mineinbox.data;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by i035921 on 2015.03.04..
  */
-public class GameDBHelper extends SQLiteOpenHelper {
+public class GameDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "mineinboxgame.db";
 
-    public GameDBHelper(Context context) {
+    public GameDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        final String SQL_CREATE_BOARD_TABLE = "CREATE TABLE " + GameContract.Board.TABLE_NAME + " (" +
-                GameContract.Board._ID + " INTEGER PRIMARY KEY," +
-                GameContract.Board.COLUMN_COORD_X + " INTEGER NOT NULL, " +
-                GameContract.Board.COLUMN_COORD_Y + " INTEGER NOT NULL, " +
-                GameContract.Board.COLUMN_STATE + " INTEGER NOT NULL );";
+        final String SQL_CREATE_BOARD_TABLE = "CREATE TABLE " + GameContract.CellEntry.TABLE_NAME + " (" +
+                GameContract.CellEntry._ID + " INTEGER PRIMARY KEY," +
+                GameContract.CellEntry.COLUMN_COORD_X + " INTEGER NOT NULL, " +
+                GameContract.CellEntry.COLUMN_COORD_Y + " INTEGER NOT NULL, " +
+                GameContract.CellEntry.COLUMN_STATE + " INTEGER NOT NULL );";
 
-//        sqLiteDatabase.execSQL(SQL_CREATE_BOARD_TABLE);
+        sqLiteDatabase.execSQL(SQL_CREATE_BOARD_TABLE);
     }
 
     @Override
@@ -36,7 +35,7 @@ public class GameDBHelper extends SQLiteOpenHelper {
         // It does NOT depend on the version number for your application.
         // If you want to update the schema without wiping data, commenting out the next 2 lines
         // should be your top priority before modifying this method.
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + GameContract.Board.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + GameContract.CellEntry.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 }
